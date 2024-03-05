32f246bd63138dd691a9d3b4580eed881c6cb9c8
DavyMugo <mavenwest6@gmail.com>
2024-03-05 13:02:15 +0300

    On branch main

---
+++ plp training/first code.py
@@ -0,0 +1,11 @@
+list=['abcd',7867,3.45,'john',23.5]
+tinylist=[123,'john']
+print(tinylist)
+print(list[2:3])
+print(list +tinylist)
+print(tinylist*5)
+tuple="abccd",657,2.345,"John",76.2
+tinytuple="wednesday",1982,"sunday"
+print(tuple[1:3])
+print(tuple*4)
+print(tuple+tinytuple)
